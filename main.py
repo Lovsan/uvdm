bba9068da58b47ae609f006ad5aed9a99f7a04e1
Lovsan <lovster6@gmail.com>
2024-10-24 19:01:05 +0300

    Update main.py
    forces a default theme now as the other 3 themes arent ready yet.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import sys
 import os
+from app.styles import apply_stylesheet
+from themes.dark_theme import dark_theme_stylesheet
+from themes.black_theme import black_theme_stylesheet
+from themes.white_theme import white_theme_stylesheet
+from themes.default_theme import updated_stylesheet
+
+#work around for error in gridview mode. qt.gui.icc: fromIccProfile: failed minimal tag size sanity 
+os.environ["QT_ENABLE_REGEXP_JIT"] = "0"
 
 # Ensure data directory exists
 if not os.path.exists('data'):
@@ -14,6 +22,10 @@ from app.main_window import YTDLPApp
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    # Example of applying a theme based on a setting
+    app.setStyleSheet(updated_stylesheet)  # Switch to the appropriate theme
+    #apply_stylesheet(app)
     window = YTDLPApp()
+    # Apply the custom stylesheet
     window.show()
     sys.exit(app.exec_())
